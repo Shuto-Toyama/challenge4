4b46088c58b388c926f74f69f718ef3c1751652f
Stoyama <Stoyama@MacBook.local>
2021-01-25 14:16:41 +0900

    ToDoリストプログラム【 クラス】

---
+++ Task.rb
@@ -0,0 +1,14 @@
+class Task
+  attr_reader :id
+  @@count = 0
+
+  def initialize(**params)
+    @id = @@count += 1
+    @title = params[:title]
+    @content = params[:content]
+  end
+
+  def info
+    "[No.#{@id}] #{@title}:#{@content}"
+  end
+end
